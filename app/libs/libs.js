3c02f3314b8b8c49f7d689bfeda89ca458d83fdb
Rick Mitchell <rickbmitchell@gmail.com>
2017-03-25 15:40:15 -0400

    first commit: angular setup, started view

---
+++ app/libs/libs.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.module('myApp.libs', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/libs', {
+    templateUrl: 'libs/libs.html',
+    controller: 'LibsCtrl'
+  });
+}])
+
+.controller('LibsCtrl', ['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
+
+
+	$scope.scrollTo = function(id) {
+		var old = $location.hash();
+	    $location.hash('answer-bottom');
+	    $anchorScroll();
+	    $location.hash(old);
+   };
+
+   // 	$scope.scrollTo = function(id) {
+   // 		var old = $location.hash();
+   //      $location.hash('form-top');
+   //      $anchorScroll();
+   //      $location.hash(old);
+   // }
+
+	$scope.addLib = function(){
+
+		$scope.lib.$add({
+			name: $scope.name,
+			email: $scope.email,
+			phone: $scope.phone
+		}).then(function(){
+			console.log("added lib");
+
+			$scope.name = '';
+			$scope.email = '';
+			$scope.phone = '';
+		})
+	}
+}]);
